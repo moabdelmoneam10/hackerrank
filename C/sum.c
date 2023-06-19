4518bd3c051714786fc9c40b40826c11c8353aad
Mohamed <m.abdelmoneam10@gmail.com>
2023-06-19 18:10:07 +0300

    Add file sum.c

--- C/sum.c
+++ C/sum.c
@@ -2,17 +2,18 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-/**
- *main function
- *return 0 in sucess
-*/
-int main()
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-int numi1,numi2;
+int numi1, numi2;
 float numf1, numf2;
 scanf("%d %d", &numi1, &numi2);
 scanf("%f %f", &numf1, &numf2);
-printf("%d %d\n", numi1+numi2, numi1-numi2);
-printf("%.1f %.1f\n", numf1+numf2, numf1-numf2);
-return 0;
+printf("%d %d\n", numi1 + numi2, numi1 - numi2);
+printf("%.1f %.1f\n", numf1 + numf2, numf1 - numf2);
+return (0);
 }
